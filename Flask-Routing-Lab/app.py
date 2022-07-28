3ab7a3d3261e2d73070ab61a9d53dd6b05e832aa
omaroium <omaralimycan@gmail.com>
2022-07-28 18:34:50 +0300

    very high quallity code

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -10,7 +10,27 @@ app = Flask(  # Create a flask app
 # Your code should be below
 
 
+@app.route('/' ,methods=['GET','POST'])  # '/' for the default page
+def home():
+    if request.method=='POST':    
+        return redirect(url_for('product'))    
+    return render_template('home.html')
 
+
+@app.route('/product' ,methods=['GET','POST'])  # '/' for the default page
+def product():   
+    if request.method=='POST':    
+        return redirect(url_for('home'))   
+
+
+    return render_template('product.html')
+@app.route('/cart' ,methods=['GET','POST'])  # '/' for the default page
+def cart():   
+    if request.method=='POST':    
+        return redirect(url_for('home'))   
+
+
+    return render_template('cart.html')
 
 # Your code should be above
 
